427689cbcc691c2beaf7bec0fb9a455460977bf6
Aristides Paris <126259081+T2paris@users.noreply.github.com>
2026-09-14 18:37:59 +0100

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// Ponto de entrada
-if (document.readyState !== "loading") {
-    AppController.init();
-} else {
-    document.addEventListener("DOMContentLoaded", () => {
-        AppController.init();
-    });
-}
